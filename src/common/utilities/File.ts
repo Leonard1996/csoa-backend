a436a8e0d36cf5e3d74fe1dfb38c221a89e96d08
kaziu <renikaziu.rk@gmail.com>
2022-11-29 14:55:24 +0100

    rk/lowercase

--- src/common/utilities/File.ts
+++ src/common/utilities/File.ts
@@ -1,52 +1,48 @@
-require('dotenv').config();
-const fs = require('fs-extra')
+require("dotenv").config();
+const fs = require("fs-extra");
 
 interface FileInfo {
-    filePath: string;
-    type: string;
-    extension: string;
+  filePath: string;
+  type: string;
+  extension: string;
 }
 
 export class File {
+  static insertBase64Media = async (fileData: string, postFix: string): Promise<FileInfo> => {
+    const uploadPath = process.env.IMAGE_FOLDER;
 
-    static insertBase64Media = async (fileData: string, postFix: string): Promise<FileInfo>  => {
-
-        const uploadPath = process.env.IMAGE_FOLDER;
-
-        // In case the '/uploads' directoy doesn't exist
-        if (!fs.existsSync(uploadPath)) {
-            fs.mkdirSync(uploadPath);
-        }
-    
-        // Decoding the base64 image
-        const matches = fileData.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-        const type = matches[1].split("/")[0]
-        const extension = matches[1].split("/")[1];
-    
-        const bufferData = new Buffer(matches[2], 'base64');
-        const filename = new Date().getTime().toString() + '_' + postFix;
-        const filePath = `${uploadPath}${filename}.${extension}`;
-
-        await fs.writeFile(filePath, bufferData);
-
-        return  {
-            filePath: filePath,
-            type: type,
-            extension: extension
-        }
+    // In case the '/uploads' directoy doesn't exist
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath);
     }
 
-    static deleteMedia = (filePath: string) => {
+    // Decoding the base64 image
+    const matches = fileData.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    const type = matches[1].split("/")[0];
+    const extension = matches[1].split("/")[1];
 
-        if (fs.existsSync(filePath)) {
-            fs.unlinkSync(filePath);
-        }
-    }
+    const bufferData = new Buffer(matches[2], "base64");
+    const filename = new Date().getTime().toString() + "_" + postFix;
+    const filePath = `${uploadPath}${filename}.${extension}`;
 
-    static getFileExtension = (filename:string):string => {
-        
-        const nameParts = filename.split('.');
-        
-        return nameParts[nameParts.length-1];
+    await fs.writeFile(filePath, bufferData);
+
+    return {
+      filePath: filePath,
+      type: type,
+      extension: extension,
+    };
+  };
+
+  static deleteMedia = (filePath: string) => {
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
     }
+  };
+
+  static getFileExtension = (filename: string): string => {
+    const nameParts = filename.split(".");
+
+    return nameParts[nameParts.length - 1].toLowerCase();
+  };
 }
